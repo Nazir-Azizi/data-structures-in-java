231acfb28da248e1e2ec18e0abb655e427c6e945
Nazir <nazirazizis123456@gmail.com>
2024-12-10 16:43:05 +0430

    added mehtods in LinkedListTest class and updated pom.xml
    changed the java version from 7 to 21 in xml file

--- src/test/java/com/data_structures/LinkedList/LinkedListTest.java
+++ src/test/java/com/data_structures/LinkedList/LinkedListTest.java
@@ -1,5 +1,7 @@
 package com.data_structures.LinkedList;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -13,8 +15,27 @@ public class LinkedListTest
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void getSize_whenEmpty_shouldReturnZero(){
+        LinkedList<Integer> list = new LinkedList<>();
+        assertEquals(0, list.getSize());
+    }
+    @Test
+    public void isEmpty_whenEmpty_shouldReturnTrue(){
+        LinkedList<Integer> list = new LinkedList<>();
+        assertTrue(list.isEmpty());
+    }
+    @Test
+    public void contains_whenNotPresent_shouldReturnFalse(){
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addLast(1);
+        list.addLast(2);
+        assertFalse(list.contains(5));
+    }   
+    @Test
+    public void contains_whenPresent_shouldReturnTrue(){
+        LinkedList<Integer> list = new LinkedList<>();
+        list.addLast(1);
+        list.addLast(2);
+        assertTrue(list.contains(1));
     }
 }
